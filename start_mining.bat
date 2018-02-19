e060dca37c2bbb9ea6087279441216ba6e3c6826
ballprogrammer <ballprogrammer@gmail.com>
2018-02-19 13:29:40 +0700

    Update start_mining.bat
    update phi-phi-pool

--- start_mining.bat
+++ start_mining.bat
@@ -1,8 +1,12 @@
+::tpruvot 224         ccminer-x64-2.2.4-cuda9
+
+
 :loop
 for /f "skip=1 delims=" %%A in ('wmic computersystem get name') do for /f "delims=" %%B in ("%%A") do set "RigsNo=%%~nxB"
 ::ccminer-818-cuda91-x64\ccminer -a neoscrypt -o stratum+tcp://tiny-pool.com:4234 -u EbcCkwZrWGKX9GgHWrKC3tvV6KKzkMCCJR.%RigsNo% -p c=END
 ccminer-818-cuda91-x64\ccminer -a neoscrypt -o stratum+tcp://tiny-pool.com:4233 -u c22z1dxuj6BvueFBtNJJr8RuKKtTWKxVbD.%RigsNo% -p c=CROP,d=1024 
-::ccminer-alexis78-ms2013-cuda7.5\ccminer -a neoscrypt -o stratum+tcp://tiny-pool.com:4234 -u EbcCkwZrWGKX9GgHWrKC3tvV6KKzkMCCJR.rig1 -p c=END
-
+::ccminer-alexis78-ms2013-cuda7.5\ccminer -a neoscrypt -o stratum+tcp://tiny-pool.com:4234 -u EbcCkwZrWGKX9GgHWrKC3tvV6KKzkMCCJR.%RigsNo% -p c=END 
+ccminer-alexis78-ms2013-cuda7.5\ccminer.exe -r 0 -a c11 -o stratum+tcp://pool1.phi-phi-pool.com:3573 -u 3NErbLfunbw3n7i2Vzrkjz6E3wruhSTZQx.%RigsNo% -p c=BTC
+::ccminer-818-cuda91-x64\ccminer.exe -i 17 -a neoscrypt -o stratum+tcp://pool1.phi-phi-pool.com:4233 -u 3NErbLfunbw3n7i2Vzrkjz6E3wruhSTZQx.%RigsNo% -p c=BTC
 timeout /T 10
 goto loop
